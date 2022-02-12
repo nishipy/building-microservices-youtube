720ef08bbf973dcaf3729aa84cac4f395fbbdbe8
nishipy <goodisonev@gmail.com>
2022-02-13 00:54:59 +0900

    Add code in episode 6

---
+++ data/product_test.go
@@ -0,0 +1,15 @@
+package data
+
+import "testing"
+
+func TestChecksValidation(t *testing.T) {
+	p := &Product{
+		Name:  "nishipys",
+		Price: 1.00,
+		SKU:   "abs-abc-def",
+	}
+	err := p.Validate()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
