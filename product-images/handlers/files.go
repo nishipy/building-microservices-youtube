7be670b7405f4244c76584bdf25453ac4ef2a606
nishipy <goodisonev@gmail.com>
2022-02-19 20:02:55 +0900

    update

--- product-images/handlers/files.go
+++ product-images/handlers/files.go
@@ -48,6 +48,7 @@ func (f *Files) invalidURI(uri string, rw http.ResponseWriter) {
 func (f *Files) saveFile(id, path string, rw http.ResponseWriter, r *http.Request) {
 	f.log.Info("Save file for product", "id", id, "path", path)
 
+	// create file path
 	fp := filepath.Join(id, path)
 	err := f.store.Save(fp, r.Body)
 	if err != nil {
