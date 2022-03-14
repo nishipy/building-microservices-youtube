f627f09d92b5c43db4cade21bbb3c1731bcd9b63
nishipy <goodisonev@gmail.com>
2022-03-15 01:06:57 +0900

    Add code for gRPC in episode 13

--- currency/go.mod
+++ currency/go.mod
@@ -3,9 +3,13 @@ module github.com/nishipy/building-microservices-youtube/currency
 go 1.17
 
 require (
+	github.com/fatih/color v1.7.0 // indirect
 	github.com/fullstorydev/grpcurl v1.8.6 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/hashicorp/go-hclog v1.2.0 // indirect
 	github.com/jhump/protoreflect v1.10.3 // indirect
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
 	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
 	golang.org/x/sys v0.0.0-20200331124033-c3d80250170d // indirect
 	golang.org/x/text v0.3.7 // indirect
--- currency/main.go
+++ currency/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"net"
+	"os"
 
+	hclog "github.com/hashicorp/go-hclog"
+	protos "github.com/nishipy/building-microservices-youtube/currency/protos/currency"
+	"github.com/nishipy/building-microservices-youtube/currency/server"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	log := hclog.Default()
+
+	gs := grpc.NewServer()
+	cs := server.NewCurrency(log)
+
+	protos.RegisterCurrencyServer(gs, cs)
+
+	reflection.Register(gs)
+
+	l, err := net.Listen("tcp", ":9092")
+	if err != nil {
+		log.Error("Unable to listen", "error", err)
+		os.Exit(1)
+	}
+	gs.Serve(l)
 }
