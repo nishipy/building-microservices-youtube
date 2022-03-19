45dbe28a940f08d1c1f9496b9826590e560dfa1b
nishipy <goodisonev4@gmail.com>
2022-03-19 15:11:28 +0900

    Update currency service in episode 14

--- currency/server/currency.go
+++ currency/server/currency.go
@@ -15,8 +15,9 @@ func NewCurrency(l hclog.Logger) *Currency {
 	return &Currency{l}
 }
 
+// GetRate implements the CurrencyServer GetRate method and returns the currency exchange rate
+// for the two given currencies.
 func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos.RateResponse, error) {
 	c.log.Info("Handle GetRate", "base", rr.GetBase(), "destination", rr.GetDestination())
-
 	return &protos.RateResponse{Rate: 0.5}, nil
 }
